c901b556d4b3ef8105edb80b0d664103fdb7cbe9
Ekokurniadi <ekokurniadi.02@gmail.com>
2021-12-01 02:12:53 +0700

    change structure

--- terbilang.go
+++ terbilang.go
@@ -5,27 +5,31 @@ import (
 	"strings"
 )
 
-type Terbilang struct {
+type terbilang struct {
 }
 
-func (terbilang *Terbilang) numberToText(inputValue int64) string {
+func Init() *terbilang {
+	return &terbilang{}
+}
+
+func (terbilang *terbilang) numberToText(inputValue int64) string {
 	value := math.Abs(float64(inputValue))
 	huruf := []string{
 		"", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas",
 	}
 	temp := ""
 	if value < 12 {
-		temp = " " + huruf[int64(value)]
+		temp = "" + huruf[int64(value)]
 	} else if value < 20 {
 		temp = terbilang.numberToText(int64(value)-10) + " belas"
 	} else if value < 100 {
 		temp = terbilang.numberToText(int64(value)/10) + " puluh " + terbilang.numberToText(int64(value)%10)
 	} else if value < 200 {
-		temp = " seratus" + terbilang.numberToText(int64(value)-100)
+		temp = " seratus " + terbilang.numberToText(int64(value)-100)
 	} else if value < 1000 {
-		temp = terbilang.numberToText(int64(value)/100) + " ratus" + terbilang.numberToText(int64(value)%100)
+		temp = terbilang.numberToText(int64(value)/100) + " ratus " + terbilang.numberToText(int64(value)%100)
 	} else if value < 2000 {
-		temp = " seribu" + terbilang.numberToText(int64(value)-1000)
+		temp = " seribu " + terbilang.numberToText(int64(value)-1000)
 	} else if value < 1000000 {
 		temp = terbilang.numberToText(int64(value)/1000) + " ribu" + terbilang.numberToText(int64(value)%1000)
 	} else if value < 1000000000 {
@@ -39,7 +43,7 @@ func (terbilang *Terbilang) numberToText(inputValue int64) string {
 	return temp
 }
 
-func (terbilang *Terbilang) Generate(inputValue int64) string {
+func (terbilang *terbilang) Convert(inputValue int64) string {
 	hasil := ""
 	if inputValue < 0 {
 		hasil = "minus " + strings.TrimSpace(terbilang.numberToText(inputValue))
